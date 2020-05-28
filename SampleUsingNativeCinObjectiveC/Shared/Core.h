ded10456bd3661f97f41fd7f6d5bdf9ab0e78053
ho sf <sfho@astri.org>
2020-05-28 14:38:55 +0800

    initial commit

---
+++ SampleUsingNativeCinObjectiveC/Shared/Core.h
@@ -0,0 +1,20 @@
+//
+// Created by Pawel Klapuch on 14/05/2017.
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+    
+#ifndef _Core_
+#define _Core_
+
+const char* version(void);
+    
+#endif
+
+#ifdef __cplusplus
+}
+#endif
